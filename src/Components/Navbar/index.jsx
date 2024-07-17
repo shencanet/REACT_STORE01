932a2bfab7b1914af276d0e4bc9f7da3e5e785a7
shencanet <santi.dionis@gmail.com>
2024-07-17 21:15:57 +0200

    ADD: Navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -5,9 +5,51 @@ const Navbar = () => {
         <nav>
         <ul>
             <li>
-                <NavLink to="/">Home</NavLink>
+                <NavLink to="/shopping">SHOPPING</NavLink>
             </li>
         </ul>
+        <ul>
+            <li>
+                <NavLink to="/all">ALL</NavLink>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <NavLink to="/clothes">CLOTHES</NavLink>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <NavLink to="/electronics">ELECTRONICS</NavLink>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <NavLink to="/furnitures">FURNITURES</NavLink>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <NavLink to="/others">OTHERS</NavLink>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <NavLink to="/my-orders">MY ORDERS</NavLink>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <NavLink to="/my-account">MY ACCOUNT</NavLink>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <NavLink to="/sign-in">SIGN IN</NavLink>
+            </li>
+        </ul>
+        
+
         </nav>
     );
     }
