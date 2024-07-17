d7d948065559f7d88dce6494f48b129adef4f8d7
shencanet <santi.dionis@gmail.com>
2024-07-17 21:09:25 +0200

    ADD: Navbar

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,4 @@
-import { useRoutes } from 'react-router-dom'
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
@@ -8,25 +8,24 @@ import SignIn from '../SignIn'
 import './App.css'
 
 const AppRoutes = () => {
-  let routes = useRoutes ([
-    {
-    path: '/',
-    element: <Home />
-    }
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
   ])
+
+  return routes
 }
 
 const App = () => {
   
   return (
-    <div className="bg-red-100">
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </div>
+<BrowserRouter>
+  <AppRoutes />
+</BrowserRouter>
   )
 }
 
