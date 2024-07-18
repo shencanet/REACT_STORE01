55f19fd87730d1a7601e6534190b5e0821b43b81
shencanet <santi.dionis@gmail.com>
2024-07-18 19:04:55 +0200

    ADD: Card

---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,13 @@
+const Card = ({ title, description }) => {
+  return (
+    <div>
+
+        <figure>
+            <span></span>
+            <img src="" alt=""/>
+        </figure>
+    </div>
+  );
+};
+
+export default Card;
